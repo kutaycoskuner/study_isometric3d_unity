d34bf79094e6aab35d53eb8b334b1b142e12756e
Orthrin <gronzelg@gmail.com>
2019-08-09 23:34:20 +0300

    Inventory UI & Scripts

--- Project/Assets/3-Scripts/PlayerController.cs
+++ Project/Assets/3-Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
@@ -20,6 +21,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
         // If left click
         if(Input.GetMouseButton(0))
         {
