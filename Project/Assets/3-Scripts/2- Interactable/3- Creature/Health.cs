bea6dd056cd1fd9c7582962a71ab79a000932a04
Orthrin <gronzelg@gmail.com>
2019-08-20 19:13:49 +0300

    HealthBar UI WIP

---
+++ Project/Assets/3-Scripts/2- Interactable/3- Creature/Health.cs
@@ -0,0 +1,33 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Health : MonoBehaviour
+// {
+//     [SerializeField]
+//     private int maxHealth = 100;
+//     private int currentHealth;
+
+//     private event Action<float> OnHealthPercentageChanged = delegate {};
+
+//     private void OnEnable()
+//     {
+//         currentHealth = maxHealth;
+//     }
+
+//     public void ModifyHealth (int amount)
+//     {
+//         currentHealth += amount;
+
+//         float currentHealthPercentage = (float)currentHealth / (float)maxHealth;
+//         OnHealthPercentageChanged(currentHealthPercentage);
+//     }
+
+//     private void Update ()
+//     {
+//         if(Input.GetKeyDown(KeyCode.Space))
+//         {
+//             ModifyHealth(-10);
+//         }
+//     }
+// }
---
+++ Project/Assets/3-Scripts/2- Interactable/3- Creature/HealthBar.cs
@@ -0,0 +1,41 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class HealthBar : MonoBehaviour
+// {
+//     [SerializeField]
+//     private Image foregroundImage;
+//     [SerializeField]
+//     private float updateSpeedSeconds =  0.5f;
+
+//     private void Awake()
+//     {
+//         GetComponentInParent<Health>().OnHealthPercentageChanged += HandleHealthChanged;
+//     }
+
+//     private void HealthHandleChanged(float percentage)
+//     {
+//         StartCoroutine(ChangetoPercentage(percentage));
+//     }
+
+//     private IEnumerator ChangetoPercentage(float percentage)
+//     {
+//         float preChangePertentage = [foregroundImage.fillAmount];
+//         float elapsed = 0f;
+
+//         while(elapsed < updateSpeedSeconds)
+//         {
+//             elapsed += Time.deltaTime;
+//             foregroundImage.fillAmount = Mathf.Lerp(preChangePercentage, percentage, elapsed / updateSpeedSeconds);
+//             yield return null;
+//         }
+//     }
+
+//     private void LateUpdate() 
+//     {
+//         transform.LookAt(Camera.main.transform);
+//         transform.Rotate(0,180,0);    
+//     }
+
+// }
